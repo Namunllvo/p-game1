0c2f2aa6b44585642819d4a0b2be62de6874a9e4
HYnllvo <gpalsu0812@naver.com>
2023-03-30 17:03:11 +0900

    보상 시스템

--- game_reward.py
+++ game_reward.py
@@ -1,14 +1,15 @@
 import test
 import random, time
 
-player_info = test.Player()
-name = player_info.name
-hp = player_info.hp
-max_hp = player_info.max_hp
-power = player_info.power
-experience = player_info.experience
+
 
 def reward():
+    player_info = test.Player()
+    name = player_info.name
+    # hp = player_info.hp
+    # max_hp = player_info.max_hp
+    # power = player_info.power
+    # experience = player_info.experience
     while True:
         print("몬스터 처치 성공! \n다음 층으로 올라갑니다.\n올라가기 전에 보상을 선택해 주세요.")
         print('1. 체력회복 2. 마나회복 3. 경험치 획득 4. 아이템 획득 5.???(랜덤)')
@@ -33,7 +34,7 @@ def reward():
         elif (select == 2):
             # Player.power 100% 회복
             # self.power = 200
-            print(f"{name}님의 MP가 전체 회복되었습니다.\n현재 MP {self.power = 200}")
+            print(f"{name}님의 MP가 전체 회복되었습니다.\n현재 MP {power:200}")
             time.sleep(2)
 
             break
@@ -42,7 +43,7 @@ def reward():
         elif (select == 3):
             # Player.경험치 40 획득(몬스터 3마리 잡고 올라갈때 경험치 40만큼 부여 -> )
             # self.experience += 40
-            print(f"{name}님은 경험치 40을 획득했습니다.\n현재 경험치 {self.experience}")
+            print(f"{name}님은 경험치 40을 획득했습니다.\n현재 경험치 {experience}")
             time.sleep(2)
 
             break
@@ -51,8 +52,8 @@ def reward():
         elif (select == 4):
             # 아이템 획득
             # 아이템 시스템으로 연결
-            item()
-            print(f"{name}님의 획득한 아이템(이름) 회복/획득했습니다.\ 현재 HP or mana or 경험치 self.h/self.max_hp")
+            # item()함수 or 클래스로 연결
+            print(f"{name}님은 아이템 으로 연결됩니다.")
             # 아이템 함수 호출
             time.sleep(2)
 
@@ -63,13 +64,12 @@ def reward():
             reward_list = ['체력회복', '마나회복', '경험치획득', '아이템획득']
             choice = random.choice(reward_list)
             print(choice)
-            print(type(choice))
             print(f"선택된 보상은 {choice} 입니다.")
-            for idex, value in enumerate(reward_list):
-                print(index, value)
-            return
+            for index, value in enumerate(reward_list):
+                a = list(print(index, value))
+
             time.sleep(2)
-        #
+        #return
         #     # return select == num
         #
         #     # return select == (num)
